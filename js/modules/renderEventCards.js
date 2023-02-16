ac42473bc1f6d621ddd2d1dcbf1b90f215a0b27b
saratyldum <98975412+saratyldum@users.noreply.github.com>
2023-02-16 17:04:13 +0100

    formatted date function

--- js/modules/renderEventCards.js
+++ js/modules/renderEventCards.js
@@ -1,23 +1,40 @@
 export default async function renderEventCards(events) {
 	const main = document.querySelector('.grid');
+	let formattedDate;
 
+	function formatDate(event) {
+		const options = {
+			hour: "numeric",
+			minute: "numeric",
+			day: "numeric",
+			month: "long",
+			year: "numeric",
+		 };
+	
+		const date = new Date(event.dates.start.dateTime);
+		formattedDate = new Intl.DateTimeFormat('no-NO', options).format(date)
+		}
+	
 	/**
 	 * @TODO gjør at hvert event kun synes en gang
 	 */
 	events.forEach(event => {
-		//DETTE ER  BILDENE SOM SKAL BRUKEs
-		const images = event.images.find(image => {
+		formatDate(event)
+
+		/**
+		 * finds images with aspect ration 4:3
+		 */
+		const image = event.images.find(image => {
 			return image.ratio === '4_3'
 		});
-		console.log(images);
 
 		const html = `
 		<div class="event">
 			<a href="./information.html">
-				<div class="event__image"><img src="${event.images[0].url}" alt="${event.name}"></div>
+				<div class="event__image"><img src="${image.url}" alt="${event.name}"></div>
 				<div class="event__information">
 					<p class="event__information-name">${event.name}</p>
-					<p class="event__information-date">${event.sales.public[1]}</p>
+					<p class="event__information-date">${formattedDate}</p>
 					<p class="event__information-city">${event._embedded.venues[0].city.name}</p>
 					<p class="event__information-venue">${event._embedded.venues[0].name}</p>
 				</div>
