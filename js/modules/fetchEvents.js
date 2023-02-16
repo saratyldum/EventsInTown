df8e550ab8814fe3267864d80e11fdf5476bc692
saratyldum <98975412+saratyldum@users.noreply.github.com>
2023-02-16 17:04:26 +0100

    internal comment

--- js/modules/fetchEvents.js
+++ js/modules/fetchEvents.js
@@ -1,4 +1,7 @@
 export default async function fetchEvents(city='oslo', classificationName='' ) {
+	/**
+	 * @TODO FÅ INN FLERE EVENTS??
+	 */
 	const endpoint = `https://app.ticketmaster.com/discovery/v2/events?apikey=g42f3Y5jMjzLC9covJlFAEMR3OZVw9CN&locale=*&city=${city}&classificationName=${classificationName}`;
 
 	const response = await fetch(endpoint);
