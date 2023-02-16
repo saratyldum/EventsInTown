af657d4c9a6860da10a540e404240e3f8869b25a
saratyldum <98975412+saratyldum@users.noreply.github.com>
2023-02-16 16:22:44 +0100

    sat up module structures

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 import fetchEvents from "./modules/fetchEvents.js";
+import renderEventCards from "./modules/renderEventCards.js";
 
 const events = await fetchEvents();
+
+renderEventCards(events);
