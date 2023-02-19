11f2fc64ad7154a0e67ae3d823b2bc1de91d2464
saratyldum <98975412+saratyldum@users.noreply.github.com>
2023-02-19 11:48:53 +0100

    implemented filtering based on clicked category

--- js/modules/fetchVariables.js
+++ js/modules/fetchVariables.js
@@ -1,20 +1,7 @@
 export default function fetchVariables(e) {
 	const cityValue = document.querySelector('.navigation-search__input').value;
-	const categories = document.querySelectorAll('.category');
+	const chosenCategory = e.target.id;
+	const date = '';
 
-	let chosenCategoryA ='';
-	const chosenCategory = function handleCategoryClick(e) {
-		console.log(e.target.id)
-		chosenCategoryA = e.target.id;
-	}
-
-	categories.forEach(category => {
-		category.addEventListener('click', chosenCategory)
-	})
-	
-	
-	console.log(chosenCategoryA);
-
-	const date = ''
-	return [cityValue, chosenCategoryA, date];
+	return [cityValue, chosenCategory, date];
 }
