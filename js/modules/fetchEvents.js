6a59930a085fb11204f1f39c384bd64241b16df2
saratyldum <98975412+saratyldum@users.noreply.github.com>
2023-02-19 11:50:01 +0100

    did some refacotiring

--- js/modules/fetchEvents.js
+++ js/modules/fetchEvents.js
@@ -1,10 +1,14 @@
-export default async function fetchEvents(city='oslo', classificationName='' ) {
-	/**
-	 * @TODO FÅ INN FLERE EVENTS??
-	 */
-	const endpoint = `https://app.ticketmaster.com/discovery/v2/events?apikey=g42f3Y5jMjzLC9covJlFAEMR3OZVw9CN&locale=*&size=200&city=${city}&classificationName=${classificationName}&sort=date,asc`;
+export default async function fetchEvents(city='oslo', classificationName='', date='' ) {
+	const main = document.querySelector('.grid');
+	main.innerHTML = ''
 
-	const response = await fetch(endpoint);
+	const apiKey = 'g42f3Y5jMjzLC9covJlFAEMR3OZVw9CN'
+	const baseURL = 'https://app.ticketmaster.com/discovery/v2/';
+	const endpointEvents = `${baseURL}/events?apikey=${apiKey}&locale=*&size=200&city=${city}&classificationName=${classificationName}&sort=date,asc&startDateTime=${date}`;
+
+	const response = await fetch(endpointEvents);
+
+	//TRY CATCH
 	const result =  await response.json();
 
 	const {events} = result._embedded;
