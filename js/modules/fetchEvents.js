60d372b562ef280bd9608fa782d4317ff64a3ddf
saratyldum <98975412+saratyldum@users.noreply.github.com>
2023-02-20 12:09:52 +0100

    make fetch events only happen if grid exist

--- js/modules/fetchEvents.js
+++ js/modules/fetchEvents.js
@@ -1,16 +1,19 @@
 export default async function fetchEvents(city='oslo', classificationName='', date='' ) {
-	const main = document.querySelector('.grid');
-	main.innerHTML = ''
-
-	const apiKey = 'g42f3Y5jMjzLC9covJlFAEMR3OZVw9CN'
-	const baseURL = 'https://app.ticketmaster.com/discovery/v2/';
-	const endpointEvents = `${baseURL}/events?apikey=${apiKey}&locale=*&size=200&city=${city}&classificationName=${classificationName}&sort=date,asc&startDateTime=${date}`;
-
-	const response = await fetch(endpointEvents);
-
-	//TRY CATCH
-	const result =  await response.json();
-
-	const {events} = result._embedded;
-	return events;
+	const grid = document.querySelector('.grid');
+	if(grid !== null) {
+		const main = document.querySelector('.grid');
+		main.innerHTML = ''
+	
+		const apiKey = 'g42f3Y5jMjzLC9covJlFAEMR3OZVw9CN'
+		const baseURL = 'https://app.ticketmaster.com/discovery/v2/';
+		const endpointEvents = `${baseURL}/events?apikey=${apiKey}&locale=*&size=200&city=${city}&classificationName=${classificationName}&sort=date,asc&startDateTime=${date}`;
+	
+		const response = await fetch(endpointEvents);
+	
+		//TRY CATCH
+		const result =  await response.json();
+	
+		const {events} = result._embedded;
+		return events;
+	}
 }
