1267f4a31f97eff37f90e12c75c4949bbc54ea00
saratyldum <98975412+saratyldum@users.noreply.github.com>
2023-02-27 12:46:26 +0100

    created local storage file for eventInformation

---
+++ js/modules/localStorage.js
@@ -0,0 +1,35 @@
+		
+
+		let eventsStorageName = "clickedEventInformation";
+		let eventInformation = getInformationFromLocal();
+					
+		export function getInformationFromLocal() {
+			const storedEventInformation = window.localStorage.getItem(eventsStorageName);
+		
+			if (storedEventInformation) {
+				return JSON.parse(storedEventInformation)
+			} else {
+				return [];
+			}
+		}
+		
+		export function addEventInformation([allEventInformation]) {
+			//creates a random string sequence to use as ID
+			for (const info of allEventInformation) {
+				const eventInformationID = Math.random().toString(32).slice(2);
+				eventInformation.push({
+					id: eventInformationID,
+					name: info,
+				});
+			}	
+		}
+		
+		export function storeInformationLocal() {
+			window.localStorage.setItem(eventsStorageName, JSON.stringify(eventInformation));
+		}
+		
+		export function clearInformationLocal() {
+			window.localStorage.clear(eventsStorageName);
+		}
+
+	
